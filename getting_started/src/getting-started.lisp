c452c9e8bb5c7f45ad7e0decb845a41edba2b843
Jacek Podkanski <ruby.object@googlemail.com>
2025-03-26 01:45:05 +0000

    loads but fail with GTK assertions

--- getting_started/src/getting-started.lisp
+++ getting_started/src/getting-started.lisp
@@ -15,7 +15,8 @@
 (define-foreign-library libgtk    (:unix (:or "libgtk-4.so"
                                               "libgtk-4.so.1")))
 (define-foreign-library libgdk     (:unix (:or "libgdk-3.so")))
-(define-foreign-library libgio     (:unix (:or "libgio-2.0.so")))
+(define-foreign-library libgio     (:unix (:or "libgio-2.0.so"
+                                               "libgio-2.0.so.0")))
 (define-foreign-library libgobject (:unix (:or "libgobject-2.0.so")))
 (define-foreign-library libglib    (:unix (:or "libglib-2.0.so")))
 (define-foreign-library libcairo   (:unix (:or "libcairo.so.2")))
@@ -24,22 +25,22 @@
 (use-foreign-library libgio)
 (use-foreign-library libgobject)
 
-(defcfun ("gtk_application_new"          :library libgtk4)
+(cffi:defcfun "gtk_application_new"
     :pointer
   (application_id :string)
   (flags :int))
 
-(defcfun "g_application_run"            ;:library libgio
+(defcfun "g_application_run"
     :int
   (application :pointer)
   (argc :int)
   (argv :string))
 
-(defcfun "g_object_unref"               ;:library libgobject
+(defcfun "g_object_unref"
     :void
   (object :pointer))
 
-(defcfun "g_signal_connect_data"        ;:library libgobject
+(defcfun "g_signal_connect_data"
     :void
   (instance :pointer)
   (detailed_signal :string)
@@ -48,10 +49,10 @@
   (destroy_data :pointer)
   (connect_flags :pointer))
 
-(defcfun "gtk_application_window_new"   ;:library libgtk
+(defcfun "gtk_application_window_new"
     :pointer)
 
-(defcfun "gtk_window_present"           ;:library libgtk
+(defcfun "gtk_window_present"
     :void
   (window :pointer))
 
@@ -67,6 +68,6 @@
                      :pointer app :string "activate" :pointer (cffi:callback activate)  :pointer (cffi:null-pointer) :void)
     (setf status (foreign-funcall "g_application_run"
                                   :pointer app :int 0 :string (null-pointer) :int))
-    (foreign-funcall "g_object_unref" :pointer app :void)
+    (foreign-funcall "g_object_unref"  :pointer app :void)
 
     status))
